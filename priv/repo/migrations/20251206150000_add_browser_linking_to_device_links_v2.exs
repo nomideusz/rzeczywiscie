9796e54be5f70123883a415917c4482893436e7f
nomideusz <b.dymet@gmail.com>
2025-12-06 16:06:14 +0100

    Re-create browser linking migration with newer timestamp
    - Use add_if_not_exists/create_if_not_exists for idempotency
    - New timestamp: 20251206150000

--- priv/repo/migrations/20251206150000_add_browser_linking_to_device_links_v2.exs
+++ priv/repo/migrations/20251206150000_add_browser_linking_to_device_links_v2.exs
@@ -1,24 +1,25 @@
-defmodule Rzeczywiscie.Repo.Migrations.AddBrowserLinkingToDeviceLinks do
+defmodule Rzeczywiscie.Repo.Migrations.AddBrowserLinkingToDeviceLinksV2 do
   use Ecto.Migration
 
   def change do
+    # Add new columns if they don't exist
     alter table(:friends_device_links) do
-      add :browser_id, :string
-      add :link_code, :string
-      add :link_code_expires_at, :utc_datetime
+      add_if_not_exists :browser_id, :string
+      add_if_not_exists :link_code, :string
+      add_if_not_exists :link_code_expires_at, :utc_datetime
     end
 
-    # Drop old unique constraint on device_fingerprint
+    # Drop old unique constraint on device_fingerprint (if exists)
     drop_if_exists unique_index(:friends_device_links, [:device_fingerprint])
 
     # Create new unique constraint on browser_id
-    create unique_index(:friends_device_links, [:browser_id])
+    create_if_not_exists unique_index(:friends_device_links, [:browser_id])
 
     # Index for looking up by link code
-    create index(:friends_device_links, [:link_code])
+    create_if_not_exists index(:friends_device_links, [:link_code])
 
     # Index for looking up by master_user_id (to find all linked browsers)
-    create index(:friends_device_links, [:master_user_id])
+    create_if_not_exists index(:friends_device_links, [:master_user_id])
 
     # Migrate existing data: set browser_id = device_fingerprint for existing records
     execute """
